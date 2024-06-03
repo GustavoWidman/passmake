86c3bf32a72152656ae55bb78b8bb5ae31453b66
GustavoWidman (laptop-arch) <gustavo.widman@sou.inteli.edu.br>
2024-06-03 16:03:32 -0300

    stop using unsafe math/rand and switch to crypto/rand

--- src/utils/password.go
+++ src/utils/password.go
@@ -1,11 +1,24 @@
 package utils
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
+// GeneratePassword generates a secure random password of the specified length using the given charset.
 func GeneratePassword(length uint8, charset string) string {
 	b := make([]byte, length)
+	charsetLength := big.NewInt(int64(len(charset)))
+
 	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+		num, err := rand.Int(rand.Reader, charsetLength)
+		if err != nil {
+			// Handle the error appropriately in your application context.
+			// For example: log the error and return an empty string.
+			return ""
+		}
+		b[i] = charset[num.Int64()]
 	}
+
 	return string(b)
 }
